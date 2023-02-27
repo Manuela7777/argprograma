a0d641f85251f7211dc9e5e3fa82c20bdd4e6ba6
Ian Link <tomasf23@gmail.com>
2023-02-27 17:18:59 -0300

    Seagrega el proyecto

---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+document.getElementById ('boton').onclick=function ()   {
+    console.log ("Su mensaje ha sido enviado");
+    document.getElementById("demo").innerHTML ="Su mensaje ha sido enviado con éxito"
+}
